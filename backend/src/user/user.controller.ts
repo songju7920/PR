666fe198a0cba1febc414d8568d381deff61b52d
오송주 <dhthdwn7920@gmail.com>
2023-11-30 21:39:07 +0900

    ✨ :: signup 기능개발

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { loginDto } from './dto/login.dto';
+import { SignupDto } from './dto/signup.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('/signup')
+  async signup(@Body() signupDto: SignupDto) {
+    const data = await this.userService.signUp(signupDto);
+
+    return Object.assign({
+      data,
+      statusCode: 201,
+      message: '유저 생성 성공',
+    });
+  }
