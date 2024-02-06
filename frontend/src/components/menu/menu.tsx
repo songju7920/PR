15817aa3857e1a39496861f4c2af02a900636cae
오송주 <dhthdwn7920@gmail.com>
2024-02-06 20:52:57 +0900

    ✨ :: 메뉴 현재 창 표시하도록 설정

--- frontend/src/components/menu/menu.tsx
+++ frontend/src/components/menu/menu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style.ts";
 import { ReactComponent as PRLogo } from "../../assets/img/icons/logo.svg";
 import { ReactComponent as Dashboard } from "../../assets/img/icons/dashboard.svg";
@@ -6,9 +6,37 @@ import { ReactComponent as Projects } from "../../assets/img/icons/projects.svg"
 import { ReactComponent as Chat } from "../../assets/img/icons/chat.svg";
 import { ReactComponent as Request } from "../../assets/img/icons/request.svg";
 import { ReactComponent as Logout } from "../../assets/img/icons/logout.svg";
-import { Link } from "react-router-dom";
+import { ReactComponent as CDashboard } from "../../assets/img/icons/Cdashboard.svg";
+import { ReactComponent as CProjects } from "../../assets/img/icons/Cproject.svg";
+import { ReactComponent as CChat } from "../../assets/img/icons/Cchat.svg";
+import { ReactComponent as CRequest } from "../../assets/img/icons/Crequest.svg";
+import { useNavigate, Link } from "react-router-dom";
 
 function Menu() {
+  const [activeIcon, setActiveIcon] = useState("dashboard");
+  const navigate = useNavigate();
+
+  const handleClick = (iconType: string) => {
+    setActiveIcon(iconType);
+    switch (iconType) {
+      case "dashboard":
+        navigate("/home");
+        break;
+      case "project":
+        navigate("/projects");
+        break;
+      case "chat":
+        navigate("/chat");
+        break;
+      case "requests":
+        navigate("/requests");
+        break;
+      case "my_project":
+        navigate("/myProject");
+        break;
+    }
+  };
+
   return (
     <S.Container>
       <S.UpperMenu>
@@ -16,43 +44,38 @@ function Menu() {
           <PRLogo />
         </S.LogoWrapper>
         <S.MenuListContainer>
-          <Link to="/home" style={{ textDecoration: "none" }}>
-            <S.MenuContainer>
-              <Dashboard width="18" height="18" />
-              <S.MenuName>dashbaord</S.MenuName>
-            </S.MenuContainer>
-          </Link>
-          <Link to="/projects" style={{ textDecoration: "none" }}>
-            <S.MenuContainer>
-              <Projects width="18" height="18" />
-              <S.MenuName>projects</S.MenuName>
-            </S.MenuContainer>
-          </Link>
-          <Link to="/chat" style={{ textDecoration: "none" }}>
-            <S.MenuContainer>
-              <Chat width="18" height="18" />
-              <S.MenuName>chatting</S.MenuName>
-            </S.MenuContainer>
-          </Link>
-          <Link to="/requests" style={{ textDecoration: "none" }}>
-            <S.MenuContainer>
-              <Request width="18" height="18" />
-              <S.MenuName>requests</S.MenuName>
-            </S.MenuContainer>
-          </Link>
-          <Link to="/myproject" style={{ textDecoration: "none" }}>
-            <S.MenuContainer>
-              <Projects width="18" height="18" />
-              <S.MenuName>dashbaord</S.MenuName>
-            </S.MenuContainer>
-          </Link>
+          <S.MenuContainer isActive={activeIcon === "dashboard"} onClick={() => handleClick("dashboard")}>
+            {activeIcon === "dashboard" ? <CDashboard /> : <Dashboard />}
+            <S.MenuName isActive={activeIcon === "dashboard"}>dashbaord</S.MenuName>
+            {activeIcon === "dashboard" && <S.SelectIcon />}
+          </S.MenuContainer>
+          <S.MenuContainer isActive={activeIcon === "project"} onClick={() => handleClick("project")}>
+            {activeIcon === "project" ? <CProjects /> : <Projects />}
+            <S.MenuName isActive={activeIcon === "project"}>projects</S.MenuName>
+            {activeIcon === "project" && <S.SelectIcon />}
+          </S.MenuContainer>
+          <S.MenuContainer isActive={activeIcon === "chat"} onClick={() => handleClick("chat")}>
+            {activeIcon === "chat" ? <CChat /> : <Chat />}
+            <S.MenuName isActive={activeIcon === "chat"}>chatting</S.MenuName>
+            {activeIcon === "chat" && <S.SelectIcon />}
+          </S.MenuContainer>
+          <S.MenuContainer isActive={activeIcon === "requests"} onClick={() => handleClick("requests")}>
+            {activeIcon === "requests" ? <CRequest /> : <Request />}
+            <S.MenuName isActive={activeIcon === "requests"}>requests</S.MenuName>
+            {activeIcon === "requests" && <S.SelectIcon />}
+          </S.MenuContainer>
+          <S.MenuContainer isActive={activeIcon === "my_project"} onClick={() => handleClick("my_project")}>
+            {activeIcon === "my_project" ? <CProjects /> : <Projects />}
+            <S.MenuName isActive={activeIcon === "my_project"}>my project</S.MenuName>
+            {activeIcon === "my_project" && <S.SelectIcon />}
+          </S.MenuContainer>
         </S.MenuListContainer>
       </S.UpperMenu>
       <S.LowerMenu>
         <Link to="/" style={{ textDecoration: "none" }}>
-          <S.MenuContainer>
+          <S.MenuContainer isActive={false}>
             <Logout width="18" height="18" />
-            <S.MenuName>log out</S.MenuName>
+            <S.MenuName isActive={false}>log out</S.MenuName>
           </S.MenuContainer>
         </Link>
       </S.LowerMenu>
--- frontend/src/components/menu/style.ts
+++ frontend/src/components/menu/style.ts
@@ -12,10 +12,10 @@ export const UpperMenu = styled.div`
   align-items: center;
   height: 92vh;
   background-color: #e3e8ee;
+  padding: 0px;
 `;
 
 export const LowerMenu = styled.div`
-  padding-left: 25px;
   height: 8vh;
   background-color: #f8f7f5;
   display: flex;
@@ -30,22 +30,36 @@ export const LogoWrapper = styled.div`
 `;
 
 export const MenuListContainer = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  justify-content: flex-start;
 `;
 
-export const MenuContainer = styled.div`
-  margin-right: 40px;
+export const MenuContainer = styled.div<{ isActive: boolean }>`
+  padding-left: 40px;
+  width: 210px;
   height: 50px;
   display: flex;
-  justify-content: space-around;
+  justify-content: start;
   align-items: center;
+  background-color: ${({ isActive }) => (isActive ? "#DBE0E9" : "none")};
 `;
 
-export const MenuName = styled.p`
+export const MenuName = styled.p<{ isActive: boolean }>`
   font-size: 20px;
   font-weight: 500;
-  color: #817e7e;
+  width: 95px;
+  color: ${({ isActive }) => (isActive ? "black" : "#817e7e")};
   margin-left: 15px;
+`;
+
+export const SelectIcon = styled.div`
+  height: 50px;
+  width: 10px;
+  background-color: #4e5686;
+  margin-left: 60px;
+  border-radius: 0px 6px 6px 0px;
+  z-index: 999;
 `;
