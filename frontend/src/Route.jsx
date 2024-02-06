155a60fd8611771edb56f9fb073ecbd5d4f4c5f2
오송주 <dhthdwn7920@gmail.com>
2024-02-06 20:53:17 +0900

    ✨ :: 메인페이지 임시 완성

--- frontend/src/Route.jsx
+++ frontend/src/Route.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login";
 import SignUp from "./pages/signup";
+import Home from "./pages/HomePage/home.tsx";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />}></Route>
-      <Route path="/Signup" element={<SignUp />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/home" element={<Home />}></Route>
     </Routes>
   );
 };
