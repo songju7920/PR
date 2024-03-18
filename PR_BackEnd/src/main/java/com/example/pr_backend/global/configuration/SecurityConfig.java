b6cf6cbfd84410604f11f43b15fcae68584bc7d1
오송주 <dhthdwn7920@gmail.com>
2024-03-18 15:16:59 +0900

    🔒️ :: create Security Config

---
+++ PR_BackEnd/src/main/java/com/example/pr_backend/global/configuration/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.pr_backend.global.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.net.http.HttpRequest;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+
+            // URL 접근 제어
+            .authorizeHttpRequests((authorize) -> authorize
+                    .requestMatchers("/user/login", "/user/signup", "/error").permitAll()
+                     .anyRequest().authenticated())
+
+            .logout((logout) -> logout
+                    .logoutSuccessUrl("/login")
+                    .invalidateHttpSession(true))
+
+            .sessionManagement((session) -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+            // headers SOP
+            .headers((headers) -> headers
+                    .frameOptions((HeadersConfigurer.FrameOptionsConfig::sameOrigin)));
+
+        return http.build();
+    }
+}
