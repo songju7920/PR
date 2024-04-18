dd6a3e116fffee5de72e731fe52d388f2fb0f3da
오송주 <dhthdwn7920@gmail.com>
2024-04-18 16:11:48 +0900

    👀 :: header 코드 수정
    - 프로필 클릭시 자신의 대시보드로 이동하는 기능 추가
    - 아이콘들 각각 링크로 연결
    - 해더 크기 조정

--- PR_FrontEnd/src/components/header/header.tsx
+++ PR_FrontEnd/src/components/header/header.tsx
@@ -3,25 +3,33 @@ import searchIcon from "../../asset/img/searchIcon.svg";
 import chattingIcon from "../../asset/img/chattingIcon.svg";
 import alarmIcon from "../../asset/img/alarmIcon.svg";
 import testProfile from "../../asset/img/test profile.png";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const onClick = () => {
-    alert("hello!");
+  const navigate = useNavigate();
+
+  const onClick = (movePage) => {
+    navigate(`/${movePage}`);
   };
 
   return (
-    <div className="flex items-center justify-around pl-[3rem] pr-[1rem] w-[94.5rem] h-[4rem]">
+    <div className="flex items-center justify-around pl-[3rem] pr-[1rem] w-full h-[8rem]">
       <div className="relative">
         <img src={searchIcon} className="absolute -left-3 hover:cursor-pointer" />
         <input placeholder="search projects" className="px-6 w-[40rem] h-[2rem] outline-none" autoComplete="false" spellCheck="false" />
       </div>
-      <div className="flex justify-center items-center w-[12rem] h-[2.5rem] bg-gradient-to-r from-indigo-500 to-sky-400 rounded-lg hover:cursor-pointer" onClick={onClick}>
+      <div className="flex justify-center items-center w-[12rem] h-[2.5rem] bg-gradient-to-r from-indigo-500 to-sky-400 rounded-lg hover:cursor-pointer" onClick={() => onClick("make-project")}>
         <p className="font-semibold text-white">make project</p>
       </div>
-      <img src={chattingIcon} />
-      <img src={alarmIcon} />
+      <img src={chattingIcon} className="hover:cursor-pointer" onClick={() => onClick("chatting")} />
+      <img src={alarmIcon} className="hover:cursor-pointer" onClick={() => onClick("alarms")} />
       <div className="w-0.5 h-[3rem] bg-slate-400"></div>
-      <div className="flex items-center w-[10rem]">
+      <div
+        className="flex items-center w-[10rem] hover:cursor-pointer"
+        onClick={() => {
+          onClick(`dashboard/${localStorage.getItem("userId")}`);
+        }}
+      >
         <img className="mr-5 w-[2.5rem] h-[2.5rem] rounded-full" src={testProfile} />
         <p>Test User</p>
       </div>
--- PR_FrontEnd/tailwind.config.js
+++ PR_FrontEnd/tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
         gray2: "#817E7E",
         gray3: "#DBE0E9",
         gray4: "#A49F9F",
+        logoutBtnColor: "#F8F7F5",
+        backGroundColor: "#F9F9F9",
         purple1: "#4E5686"
       },
       fontSize: {
