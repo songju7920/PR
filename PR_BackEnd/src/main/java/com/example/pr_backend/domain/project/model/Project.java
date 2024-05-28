4c5537c6cf3e7493688b908b75c91b8de1b00441
오송주 <dhthdwn7920@gmail.com>
2024-05-28 16:51:38 +0900

    db:: develop project database entity & repository

---
+++ PR_BackEnd/src/main/java/com/example/pr_backend/domain/project/model/Project.java
@@ -0,0 +1,45 @@
+package com.example.pr_backend.domain.project.model;
+
+import com.example.pr_backend.domain.user.model.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Getter
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private long post_id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String contents;
+
+    @Column(nullable = false)
+    @ColumnDefault("false")
+    private Boolean is_recruiting;
+
+    @ManyToOne
+    @JoinColumn(name = "writer_id")
+    private User user;
+
+    protected Project() {}
+
+    @Builder
+    public Project(String title, String contents, Boolean is_recruiting, User user) {
+        this.title = title;
+        this.contents = contents;
+        this.user = user;
+        this.is_recruiting = is_recruiting;
+    }
+}
---
+++ PR_BackEnd/src/main/java/com/example/pr_backend/domain/project/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.example.pr_backend.domain.project.repository;
+
+import com.example.pr_backend.domain.project.model.Project;
+import org.springframework.data.repository.Repository;
+
+public interface ProjectRepository extends Repository<Project, Long> {
+}
